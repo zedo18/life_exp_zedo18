b279711c6226eb941230fb9ee1461a106e0d34b7
Zeynep Yilmaz <zy6372a@american.edu>
2026-09-13 13:38:21 -0400

    Solve problem 2

--- homework_1/homework_1_zeynep_yilmaz.R
+++ homework_1/homework_1_zeynep_yilmaz.R
@@ -1,10 +1,13 @@
 
-title: "DATA413 Homework 1"
-author: "Zeynep Yilmaz"
+#Homework 1/ Zeynep Yilmaz/ DATA413
 
 # Problem 1
 scores <- c(72, 85, 91, 68, 84)
 mean(scores)
 
 
+#Problem 2
+
+sd(scores)
+median(scores)
 
